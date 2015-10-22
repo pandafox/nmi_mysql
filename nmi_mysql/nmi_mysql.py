0cb79bcef0555bcf743ccad18eac7f2b80452c87
Jasper Sibayan <sibayanjasper@gmail.com>
2015-10-22 14:39:47 +0800

    Refactor
    Remove executemany since it only chunkifies params and uses execute for
    each chunk, learn to chunkify on your own
    Make _params optional
    Remove unused imports
    Add gitignore

--- nmi_mysql/nmi_mysql.py
+++ nmi_mysql/nmi_mysql.py
@@ -6,7 +6,6 @@
 import re
 import logging
 import pymysql.cursors
-from datetime import datetime
 
 
 class DB():
@@ -60,46 +59,28 @@ class DB():
 
         return None
 
-    def query(self, _query, _params):
+    def query(self, _query, _params=None):
         """
             self.handle holds the connection
             _query is the query
             _params holds the variables need by the query
         """
 
-        """
-            replace all instances of ? to %s la :D YOLO programming best!
-        """
-
-        query = re.sub("\?", "%s", _query)
         result = None
+        query = _query
 
-        if isinstance(_params, list):
-            params = []
-            values = []
-            for param in _params:
-                if isinstance(param, tuple):
-                    values.append('(' + self.to_string(param) + ')')
-
-                else:
-                    params.append(self.to_string(param))
-
-            if values:
-                params = ','.join(values)
-                query = query % params[1:-1]
-
-            else:
-                query = query % tuple(params)
-
-        else:
-            query = query % self.to_string(_params)
+        if _params:
+            query = self.generate_query(_query, _params)
 
         try:
             with self.handle.cursor() as cursor:
                 cursor.execute(query, ())
-                
+
                 if 'insert' in query.lower() or 'update' in query.lower():
-                    result = { 'affected_rows': cursor.rowcount }
+                    result = {
+                        'affected_rows': cursor.rowcount
+                    }
+
                 else:
                     result = list(cursor.fetchall())
 
@@ -111,15 +92,33 @@ class DB():
 
         return result
 
-    def execute_many(self, _query, _params):
-        try:
-            with self.handle.cursor() as cursor:
-                cursor.executemany(_query, _params)
-        except Exception as err:
-            self.logger.warn(err)
-            return None
+    def generate_query(self, _query, _params):
+        """
+            replace all instances of ? to %s la :D YOLO programming best!
+        """
 
-        self.handle.commit()
+        query = re.sub('\?', '%s', _query)
+
+        if not isinstance(_params, list):
+            return query % self.to_string(_params)
+
+        params = []
+        values = []
+        for param in _params:
+            if isinstance(param, tuple):
+                values.append('(' + self.to_string(param) + ')')
+
+            else:
+                params.append(self.to_string(param))
+
+        if values:
+            params = ','.join(values)
+            query = query % params[1:-1]
+
+        else:
+            query = query % tuple(params)
+
+        return query
 
     def to_string(self, temp):
         if isinstance(temp, (list, tuple)):
